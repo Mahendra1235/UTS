9e44186cc2882f6c740534182fafb22e202f545e
Mahendra <mahendrathungha.99@gmail.com.com>
2025-09-05 13:49:55 +0530

    Final changes

--- src/Pages/Solutions.js
+++ src/Pages/Solutions.js
@@ -1,14 +1,14 @@
 import React from "react";
-import SolutionsImage from "../assets/bg1.webp"
+import SolutionsImage from "../assets/uc.jpg";
 
 export default function Solutions() {
   return (
-    <div>
+    <div className="solution-container">
       <img
-              src={SolutionsImage}
-              alt="Our solution"
-              className="solution-image"
-            />
+        src={SolutionsImage}
+        alt="Our solution"
+        className="solution-image"
+      />
     </div>
   );
 }
